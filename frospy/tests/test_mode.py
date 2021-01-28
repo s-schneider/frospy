6aea1b40613e08210259f2f23174ad22872dc294
Simon Schneider MacBook 13 <s.a.schneider@uu.nl>
2021-01-28 11:27:25 +0100

    added mode tests

--- frospy/tests/test_mode.py
+++ frospy/tests/test_mode.py
@@ -1,5 +1,4 @@
-from frospy.core.modes import Mode
-from obspy.core import AttribDict
+from frospy.core.modes import Mode, Modes, format_name
 
 def test_mode():
     m = Mode(n=0, type='S', l=2, freq=0.309, Q=509.684)
@@ -11,4 +10,18 @@ def test_mode():
     assert m.Q == 509.684
 
 def test_modes():
-    assert 0 == 0
+    M = Modes([Mode(), Mode(), Mode()])
+    assert len(M) == 3
+
+    m1 = Modes([Mode(), Mode(), Mode()])
+    assert len(m1) == 3
+
+    m2 = Modes([Mode(), Mode()])
+    assert len(m2) == 2
+
+    M = m1 + m2
+    assert len(M) == 5
+
+def test_mode_name_formatting():
+    s = format_name('0s2', 2)
+    assert s == '00s02'

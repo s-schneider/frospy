8eebc33fa1e4376922f91b9a6ab0ad53863db1bf
Simon Schneider MacBook 13 <s.a.schneider@uu.nl>
2021-01-28 10:29:12 +0100

    WIP tests for mode class

--- frospy/tests/todo/Su/inspect.py
+++
@@ -1,7 +0,0 @@
-from frospy.preprocessing.inspect import inspect
-import sys
-
-#event = sys.argv[1] # Event name
-#event='060994A'
-event='100494B'
-inspect('300hrs/raw/%s'%event, outputformat='pickle', raw_only=False, start=3)
--- frospy/tests/todo/use_spectrum.py
+++
@@ -1,6 +0,0 @@
-from frospy.spectrum.app import spectrum
-
-data = '/quanta1/home/simons/splitting/modes/00s13/self_coupling/R-Comp/12_plus/syn/010196C.ahx.syn'
-syn = '/quanta1/home/simons/splitting/modes/00t14/self_coupling/R-Comp/12_plus/syn/010196C.ahx.syn'
-
-spectrum(data=data, syn=syn, fw=[1,2], tw=[3,60], minispec=True

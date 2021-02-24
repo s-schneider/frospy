a067904cb964e12d5cf0d92df9b370ac3792ad3e
Simon Schneider MacBook 13 <s.a.schneider@uu.nl>
2021-02-24 15:35:16 +0100

    new build

--- build/lib/frospy/core/splittingfunc/write.py
+++ build/lib/frospy/core/splittingfunc/write.py
@@ -9,22 +9,37 @@ def write(SF, filename='cst-coef', format='dat', coupling='self',
     http://www.geo.uu.nl/~deuss/img/cst-coef.dat
     """
     if format == 'dat':
-        if coupling.lower() in ['self', 'sc']:
-            cmod = 'sc'
-            msg="""Self-coupled splitting function coefficients in micro Hz. The first line contains the n, name, l of
-the mode.
-Then there are two lines for each angular order s of the splitting function, starting with s=0.
-The first of these contains the c_st coefficients and the second line the corresponding error values.
-The coefficients are ordered c_s0, Re(c_s1), Im(c_s1), Re(c_s2), Im(c_s2), ... etc.\n"""
+        msg = create_cst_content(SF, coupling, verbose)
+        text_file = open("{}.{}".format(filename, format), "wt")
+        _n = text_file.write(msg)
+        text_file.close()
+    return
 
-        if coupling.lower() in ['cross', 'cc', 'gc']:
-            cmod = 'cc'
-            msg="""Cross-coupled splitting function coefficients in micro Hz. The first line contains the n1, type, l1 of
-the first mode and the n2, type, l2 of the second mode. The second line contains the minimum and
-maximum angular order s of the splitting function coefficients.
-Then there are two lines for each angular order of the splitting function. The first of these
-contains the c_st coefficients and the second line the corresponding error values.
-The coefficients are ordered c_s0, Re(c_s1), Im(c_s1), Re(c_s2), Im(c_s2), ... etc.\n"""
+
+def create_cst_content(SF, coupling, verbose=False):
+    if coupling.lower() in ['self', 'sc']:
+        cmod = 'sc'
+        msg = ("Self-coupled splitting function coefficients in micro Hz. "
+               "The first line contains the n, name, l of\nthe mode."
+               "Then there are two lines for each angular order s of the "
+               "splitting function, starting with s=0.\n"
+               "The first of these contains the c_st coefficients and the "
+               "second line the corresponding error values.\n"
+               "The coefficients are ordered "
+               "c_s0, Re(c_s1), Im(c_s1), Re(c_s2), Im(c_s2), ... etc.\n")
+
+    if coupling.lower() in ['cross', 'cc', 'gc']:
+        cmod = 'cc'
+        msg = ("Cross-coupled splitting function coefficients in micro Hz. "
+               "The first line contains the n1, type, l1 of "
+               "the first mode and the n2, type, l2 of the second mode. "
+               "The second line contains the minimum and "
+               "maximum angular order s of the splitting function "
+               "coefficients.\nThen there are two lines for each angular "
+               "order of the splitting function. The first of these"
+               "contains the c_st coefficients and the second line the "
+               "corresponding error values.\n The coefficients are ordered "
+               "c_s0, Re(c_s1), Im(c_s1), Re(c_s2), Im(c_s2), ... etc.\n")
 
         for sfunc in SF:
             for mode in sfunc.cst:
@@ -39,8 +54,10 @@ The coefficients are ordered c_s0, Re(c_s1), Im(c_s1), Re(c_s2), Im(c_s2), ... e
                     if '-' not in mode:
                         continue
                     ccdegs = list(sfunc.cst[mode].keys())
-                    n1, name1, l1, _, n2, name2, l2= split_digit_nondigit(mode)
-                    msg += "{} {} {}  {} {} {}\n".format(n1, name1, l1, n2, name2, l2)
+                    sdn = split_digit_nondigit(mode)
+                    n1, name1, l1, _, n2, name2, l2 = sdn[:]
+                    msg += "{} {} {}  {} {} {}\n".format(n1, name1, l1, n2,
+                                                         name2, l2)
                     msg += "{} {}\n".format(ccdegs[0], ccdegs[-1])
                 degs = sort_human(list(sfunc.cst[mode].keys()))
                 for deg in degs:
@@ -59,9 +76,4 @@ The coefficients are ordered c_s0, Re(c_s1), Im(c_s1), Re(c_s2), Im(c_s2), ... e
                         msg += '\n'
                         msg += ' '.join(cf_errors)
                     msg += '\n'
-
-
-        text_file = open("{}.{}".format(filename, format), "wt")
-        n = text_file.write(msg)
-        text_file.close()
-    return
+    return msg

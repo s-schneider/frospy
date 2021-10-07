fd2555df5d2a59c5ef22be044b47e440952160e8
Simon Schneider MacBook 13 <s.a.schneider@uu.nl>
2021-10-07 12:07:57 +0200

    Fixed multiple model input

--- frospy/plot/branch.py
+++ frospy/plot/branch.py
@@ -282,13 +282,17 @@ def branch(ifiles=None, data_label=None, label1=None, SF_in=None,
         # if S.stats.name.split('_')[0] not in m:
         if S.stats.model not in m:
             m.append(S.stats.model)
-
+    if verbose is True:
+        print('models', m)
     if SF_in is not None:
         # if model[0] is not None: # I dont know why this is here
         #     model.append(m[1])
         if len(m) == 2 and model[0] is None:
             model = [m[-1]]  # apppend 2nd data set as 1st model
-
+        else:
+            model = m
+    if verbose is True:
+        print('models w/o data', model)
     # spacing between coeffs for the same modes,
     # only if one than one data set is plotted
     if spacing and model[0] is not None:
@@ -325,6 +329,8 @@ def branch(ifiles=None, data_label=None, label1=None, SF_in=None,
         for m in input:
             width[m] = 0
 
+    if verbose is True:
+        print('Final models ', model)
     # Prepare Figures and Plot
     # use first splitting function to check the amount of plots
     # we need 2 * degree + 1 figures
@@ -402,6 +408,8 @@ def branch(ifiles=None, data_label=None, label1=None, SF_in=None,
     markers = {}
     for _m in model:
         markers[_m] = next(markersit)
+        if verbose is True:
+            print('markers: ', _m, markers[_m])
     colors = {}
     colors_data = {}
 
@@ -532,7 +540,7 @@ def branch(ifiles=None, data_label=None, label1=None, SF_in=None,
                      s.stats.model is not None
                      ):
                     label_model_set[s.stats.model] = False
-
+                print(mlabel, mark, markers)
                 if mlabel not in mark:
                     mark[mlabel] = markers[mlabel]
                 if mlabel not in colors:

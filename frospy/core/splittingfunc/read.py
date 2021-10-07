0b1c47e5bf4fe66ff6975d0c88f9adc635058b27
Simon Schneider MacBook 13 <s.a.schneider@uu.nl>
2021-10-07 09:56:30 +0200

    Fixing read_cst

--- frospy/core/splittingfunc/read.py
+++ frospy/core/splittingfunc/read.py
@@ -115,7 +115,6 @@ def read_cst(setup=None, modes=None, cfile=None, modes_dir=None, R=-0.2,
     cst, dst, cst_errors, dst_errors = None, None, None, None
 
     if mdcplbin is not None:
-        from IPython import embed; embed()
         cst, dst = read_cst_S20RTS(modesin=modesin, modes_ccin=modes_ccin,
                                    setup=setup, modes_dst=modes_scin_dst,
                                    R=R, model=cfile, verbose=verbose,
